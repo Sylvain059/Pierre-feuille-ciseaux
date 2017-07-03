bda843f2e420c8c05ae46aed28824598739578fb
Sylvain059 <sylvain.sculier88@gmail.com>
2017-07-03 09:45:30 +0200

    add prog for game

---
+++ js/main.js
@@ -0,0 +1,58 @@
+var pointJoueur = 0;
+var pointOrdi = 0;
+
+while (pointJoueur < 3 && pointOrdi < 3) 
+	{
+	var choixUtilisateur = prompt("On va jouer au pierre, feuille, ciseaux. Choisi l'un des trois!");
+var choixOrdi = Math.random();
+if (choixOrdi < 0.34) {
+	choixOrdi = "pierre";
+} 
+else if(choixOrdi <= 0.67) {
+	choixOrdi = "feuille";
+} 
+else {
+	choixOrdi = "ciseaux";
+} 
+
+console.log("User" + ":" + " " + choixUtilisateur + " " + "/" + " " + "Ordi" + ":" + " " + choixOrdi);
+
+if (choixUtilisateur == "pierre" && choixOrdi == "ciseaux" || choixUtilisateur == "feuille" && choixOrdi == "pierre" || choixUtilisateur == "ciseaux" && choixOrdi == "feuille") {console.log("Tu as de la chance je t'ai laissé gagner!");
+pointJoueur++;
+}
+
+else if (choixUtilisateur === null) {
+console.log("Espèce de lâche, tu viens d'abandonner!!");
+break;
+}
+
+else if (choixUtilisateur == "pierre" && choixOrdi == "pierre" || choixUtilisateur == "feuille" && choixOrdi == "feuille" || choixUtilisateur == "ciseaux" && choixOrdi == "ciseaux") {console.log("Match nul, personne ne gagne!");
+}
+
+else if (choixUtilisateur == "ciseaux" && choixOrdi == "pierre" || choixUtilisateur == "pierre" && choixOrdi == "feuille" || choixUtilisateur == "feuille" && choixOrdi == "ciseaux") {console.log("Tu as perdu, c'est moi qui gagne!!!");
+pointOrdi++;
+}
+
+else if (choixUtilisateur === "" || choixUtilisateur !== "pierre" || choixUtilisateur !== "feuille" || choixUtilisateur !== "ciseaux") {
+console.log("Entre une réponse, correct");
+}
+
+
+
+console.log("User" + " " + pointJoueur);
+console.log("Ordi" + " " + pointOrdi);
+console.log("");
+}
+
+if (pointJoueur === 3) {
+console.log("Bien joué, c'est une victoire pour toi");
+}
+
+else {
+console.log("LOOSER! je t'ai eu!!!");
+}
+
+
+//Tant que user ne gagne pas 3 fois OU ordi ne gagne pas 3 fois
+//Relance le jeu
+//
